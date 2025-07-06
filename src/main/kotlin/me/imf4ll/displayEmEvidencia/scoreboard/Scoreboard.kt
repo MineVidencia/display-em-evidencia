3a192408655b0b50864b0186fd6ce15cce63f204
imf4ll <z3ox1s@protonmail.com>
2025-07-06 20:35:05 -0300

    feat: Integração do sistema de Scoreboard

--- src/main/kotlin/me/imf4ll/displayEmEvidencia/scoreboard/Scoreboard.kt
+++ src/main/kotlin/me/imf4ll/displayEmEvidencia/scoreboard/Scoreboard.kt
@@ -1,49 +1,8 @@
 package me.imf4ll.displayEmEvidencia.scoreboard
 
 import me.imf4ll.displayEmEvidencia.scoreboard.listeners.SidebarListen
-import org.bukkit.Bukkit
-import org.bukkit.plugin.java.JavaPlugin
-import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.Plugin
 
 class Scoreboard() {
-  fun run(plugin: Plugin) {
-    plugin.server.pluginManager.registerEvents(SidebarListen(plugin), plugin);
-  }
+  fun run(plugin: Plugin) = plugin.server.pluginManager.registerEvents(SidebarListen(plugin), plugin);
 }
-
-class SScoreboard : JavaPlugin()  {
-
-
-
-  companion object {
-    var econ: Economy? = null
-      private set
-  }
-
-  override fun onEnable() {
-
-    if (!setup()) {
-      logger.severe("Vault não foi iniciado ou não encontrado. Verifique a existência do vault")
-      return
-    }
-
-    Bukkit.getPluginManager().registerEvents(SidebarListen(this), this)
-
-
-
-  }
-
-  private fun setup(): Boolean {
-    if (server.pluginManager.getPlugin("Vault") == null) {
-      return false
-    }
-
-    val response =  server.servicesManager.getRegistration(Economy::class.java) ?: return false
-    econ = response.provider
-    return econ != null
-  }
-
-
-}
-
