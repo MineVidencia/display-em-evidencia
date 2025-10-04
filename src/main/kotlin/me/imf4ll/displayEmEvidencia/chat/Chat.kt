7d23a7ebe578a2fadbd22d9e1edb6327125d8f82
imf4ll <z3ox1s@protonmail.com>
2025-10-04 09:19:46 -0300

    feat: Adicionado suporte a broadcasting automatizado

--- src/main/kotlin/me/imf4ll/displayEmEvidencia/chat/Chat.kt
+++ src/main/kotlin/me/imf4ll/displayEmEvidencia/chat/Chat.kt
@@ -12,6 +12,8 @@ import me.imf4ll.displayEmEvidencia.chat.commands.UnmuteCommand
 import me.imf4ll.displayEmEvidencia.chat.listeners.ChatListener
 import me.imf4ll.displayEmEvidencia.chat.listeners.CommandListener
 import me.imf4ll.displayEmEvidencia.chat.listeners.PlayerListener
+import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 
 class Chat {
@@ -34,5 +36,27 @@ class Chat {
     plugin.getCommand("unmute")?.setExecutor(UnmuteCommand());
     plugin.getCommand("bloquear")?.setExecutor(BlockCommand());
     plugin.getCommand("lockpm")?.setExecutor(LockPrivateCommand());
+
+    loadMessages(plugin, plugin.config);
+  }
+
+  fun loadMessages(plugin: JavaPlugin, config: FileConfiguration) {
+    val messagesSection = config.getConfigurationSection("messages");
+    val messages = messagesSection?.getKeys(false)?.mapNotNull { it.toString() }?.sorted();
+
+    if (messages != null) {
+      for (m in messages) {
+        if (!messagesSection.getBoolean("$m.enabled")) continue
+
+        val time = messagesSection.getInt("$m.time");
+
+        Bukkit.getScheduler().runTaskTimer(plugin, Runnable {
+          Bukkit.broadcastMessage(m.replace("&", "§"));
+
+        },
+        0L,
+        20L * 60 * time)
+      }
+    }
   }
 }
--- src/main/kotlin/me/imf4ll/displayEmEvidencia/chat/listeners/CommandListener.kt
+++ src/main/kotlin/me/imf4ll/displayEmEvidencia/chat/listeners/CommandListener.kt
@@ -2,7 +2,6 @@ package me.imf4ll.displayEmEvidencia.chat.listeners
 
 import me.imf4ll.displayEmEvidencia.services.CooldownService
 import me.imf4ll.displayEmEvidencia.services.Hooks
-import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
@@ -27,33 +26,19 @@ class CommandListener(val plugin: Plugin) : Listener {
   @EventHandler
   fun onCommand(e: PlayerCommandPreprocessEvent) {
     val player = e.player;
-    val command = Bukkit.getPluginCommand(e.message.split(" ")[0].lowercase().removePrefix("/"));
     val cooldowned = CooldownService.commandCooldown;
 
-    if (command != null) {
-      if (!player.isOp) {
-        if (cooldowned.contains(player.uniqueId)) {
-          e.isCancelled = true;
-
-          player.sendMessage("§c§lERRO:§r §cEspere um momento para enviar outro comando.");
-
-        } else {
-          val cooldown = Hooks.config.getInt("command-cooldown");
-
-          CooldownService.addCommandCooldown(player.uniqueId, cooldown);
-        }
-      }
-
-      if (command.permission != null && !player.hasPermission(command.permission!!)) {
+    if (!player.isOp) {
+      if (cooldowned.contains(player.uniqueId)) {
         e.isCancelled = true;
 
-        player.sendMessage("§c§lERRO:§r §cVocê não tem permissão para isso.");
+        player.sendMessage("§c§lERRO:§r §cEspere um momento para enviar outro comando.");
+
+      } else {
+        val cooldown = Hooks.config.getInt("command-cooldown");
+
+        CooldownService.addCommandCooldown(player.uniqueId, cooldown);
       }
-
-    } else {
-      e.isCancelled = true;
-
-      player.sendMessage("§c§lERRO:§r §cEsse comando não existe.");
     }
 
     return;
